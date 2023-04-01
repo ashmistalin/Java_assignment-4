187f1755baa9cd246335a4682b12199035206d41
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 21:57:58 +0530

    Update and rename Remove.java to Remove-Question 6.java

--- Remove-Question 6.java
+++ Remove-Question 6.java
@@ -19,3 +19,15 @@ public class Remove {
         System.out.println("Tree Set after removing the last element: " + t1);
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=11528:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi Remove
+1
+2
+3
+4
+5
+Tree Set after removing the last element: [1, 2, 3, 4]
+
+Process finished with exit code 0
