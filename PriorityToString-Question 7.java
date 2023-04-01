e22bd85c1f892a3614f3e95ea681b4fd0f378ce9
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 22:03:13 +0530

    Update and rename PriorityToString.java to PriorityToString-Question 7.java

--- PriorityToString-Question 7.java
+++ PriorityToString-Question 7.java
@@ -18,3 +18,10 @@ public class PriorityToString
         System.out.println("String Representation of the Priority Queue: " + sb);
     }
 }
+
+OUTPUT:
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=11772:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi PriorityToString
+Priority Queue: [Apple, Banana, Ice-cream, Lychee]
+String Representation of the Priority Queue: Apple Banana Ice-cream Lychee 
+
+Process finished with exit code 0
