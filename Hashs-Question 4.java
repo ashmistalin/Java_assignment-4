335bb3880a1ab195753b63d05c792b0779b827cc
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 21:42:01 +0530

    Update and rename Hashs.java to Hashs-Question 4.java

--- Hashs-Question 4.java
+++ Hashs-Question 4.java
@@ -19,3 +19,16 @@ public class Hashs {
         System.out.println(h1);
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=10931:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi Hashs
+The original hashset:
+1
+2
+3
+4
+The new hashset after removing elements:
+[]
+
+Process finished with exit code 0
