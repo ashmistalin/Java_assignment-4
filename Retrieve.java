9e56b02e7205099f9d3eb432085606f67a0a9290
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 21:33:44 +0530

    Update Retrieve.java

--- Retrieve.java
+++ Retrieve.java
@@ -17,3 +17,14 @@ public class Retrieve {
         }
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=10415:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi Retrieve
+
+First element:  Good 
+The original list:
+Good Morning to all 
+
+Process finished with exit code 0
+
