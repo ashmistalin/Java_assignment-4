78c92b5f991415004bc456a7c498fcd6c67f2ff1
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 21:40:10 +0530

    Update and rename Contains.java to Contains-Question 3.java

--- Contains-Question 3.java
+++ Contains-Question 3.java
@@ -18,3 +18,11 @@ public class Contains {
         }
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=10753:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi Contains
+
+The element 'Good' exists.
+
+Process finished with exit code 0
