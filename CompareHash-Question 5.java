12569701b59483b252552fe9337997706ce84143
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-01 21:50:16 +0530

    Update and rename CompareHash.java to CompareHash-Question 5.java

--- CompareHash-Question 5.java
+++ CompareHash-Question 5.java
@@ -28,3 +28,16 @@ public class CompareHash {
 
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=11139:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi CompareHash
+Elements of first hash set: [1, Grapes, Mango, Orange]
+Elements of second hash set: [Grapes, Strawberry, Mango, Banana]
+
+Both hash-sets are not equal.
+
+Difference between two HashSet: [1, Orange]
+Difference between two HashSet: [Grapes, Strawberry, Mango, Banana]
+
+Process finished with exit code 0
