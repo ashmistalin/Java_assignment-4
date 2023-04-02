b1ff8df947775dec4b94d0b676b09033b2ef05d8
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-02 11:17:05 +0530

    Update and rename Keyss.java to Keyss-Question 10.java

--- Keyss-Question 10.java
+++ Keyss-Question 10.java
@@ -17,3 +17,9 @@ public class Keyss {
         }
     }
 }
+
+OUTPUT:
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=13263:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi Keyss
+Key-Value mapping associated with the least key greater than or equal to 3 is : 3=Three
+
+Process finished with exit code 0
