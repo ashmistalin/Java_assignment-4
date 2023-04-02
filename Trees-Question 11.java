dfafe87e2bf85903c1a405159294667c451f6c74
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-04-02 11:25:47 +0530

    Update and rename Trees.java to Trees-Question 11.java

--- Trees-Question 11.java
+++ Trees-Question 11.java
@@ -15,3 +15,10 @@ public class Trees {
         System.out.println("Least key greater than or equal to " + key + " is " + leastKey);
     }
 }
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=13530:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\Dell\IdeaProjects\Ashmi\out\production\Ashmi Trees
+Least key greater than or equal to 5 is 5
+
+Process finished with exit code 0
